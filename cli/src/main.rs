552fa6b2e7f9ca336b0decba2891785451dfcdf0
Artur Taranchiev <ewok@ewok.ru>
2018-12-07 09:56:50 +0300

    Initial

---
+++ cli/src/main.rs
@@ -0,0 +1,19 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+fn main() {
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path).expect("couldn;t read file");
+
+        for line in content.lines()  {
+            if line.contains(&args.pattern){
+                println!("{}", line)
+            }
+        }
+}
